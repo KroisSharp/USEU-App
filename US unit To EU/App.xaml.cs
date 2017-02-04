dbe4a39c948d633994a5c026369f320e034cab51
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-02-04 13:07:35 +0100

    First Commit Klar til at kode

--- US unit To EU/App.xaml.cs
+++ US unit To EU/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using US_unit_To_EU.View;
 
 namespace US_unit_To_EU
 {
@@ -72,7 +73,7 @@ namespace US_unit_To_EU
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(HovedMenu), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
--- US unit To EU/View/Weight.xaml.cs
+++ US unit To EU/View/Weight.xaml.cs
@@ -13,16 +13,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace US_unit_To_EU
+namespace US_unit_To_EU.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Weight : Page
     {
-        public MainPage()
+        public Weight()
         {
             this.InitializeComponent();
         }
